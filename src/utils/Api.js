a9d48ba44346cddbb62b5dd052dff2c05feef792
Dudeman <seanpsemyck98@gmail.com>
2025-12-10 15:41:35 -0600

    avatar object

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -47,7 +47,7 @@ class Api {
     });
   }
 
-  editAvatarImg() {
+  editAvatarImg({ avatar }) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
